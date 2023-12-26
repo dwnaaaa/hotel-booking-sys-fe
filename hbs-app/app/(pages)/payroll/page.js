6903cfbd2cba54c3ad5b40b72566ba69cdd3198c
Francesca Maries P. Buguis <fpbuguis@up.edu.ph>
2023-12-26 16:35:54 +0800

    Added payroll page

---
+++ hbs-app/app/(pages)/payroll/page.js
@@ -0,0 +1,10 @@
+import Payroll from '@/components/Payroll/Payroll'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Payroll />
+  )
+}
+
+export default page
---
+++ hbs-app/components/Payroll/Payroll.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+
+const Payroll = () => {
+  return (
+    <Layout>
+        <div>Payroll</div>
+    </Layout>
+  )
+}
+
+export default Payroll
