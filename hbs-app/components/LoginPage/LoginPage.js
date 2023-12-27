5350ba9963614cf07407f62cb2666c5e119d6324
Francesca Maries P. Buguis <fpbuguis@up.edu.ph>
2023-12-27 13:56:52 +0800

    Update LoginPage.js

--- hbs-app/components/LoginPage/LoginPage.js
+++ hbs-app/components/LoginPage/LoginPage.js
@@ -34,3 +34,51 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
+// 'use client'
+// import { useState } from 'react';
+// import { useRouter } from 'next/router';
+
+// function LoginForm() {
+//   const router = useRouter();
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     router.push('/dashboard');
+//   };
+
+//   return (
+//     <div className="login-container">
+//       <form onSubmit={handleSubmit} className="login-form">
+//         <h2>Login</h2>
+//         <div className="input-group">
+//           <label htmlFor="username">Username</label>
+//           <input
+//             type="text"
+//             id="username"
+//             name="username"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="input-group">
+//           <label htmlFor="password">Password</label>
+//           <input
+//             type="password"
+//             id="password"
+//             name="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default LoginForm;
