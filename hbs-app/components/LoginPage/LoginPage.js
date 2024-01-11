1cb3025006a154d19d7c15d02b1fe82bcf20ab09
Ashley Ordoñez <mcordonez3@up.edu.ph>
2024-01-12 05:10:14 +0800

    Start login implementation

--- hbs-app/components/LoginPage/LoginPage.js
+++ hbs-app/components/LoginPage/LoginPage.js
@@ -1,19 +1,53 @@
 
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPage.css'; // Assuming you have a CSS file for styling
 import Layout from '../Layout/Layout';
 import { useRouter } from 'next/navigation';
+import { decode } from 'jsonwebtoken';
 
 const LoginPage = () => {
-
+  const SECRET_KEY = "7132743379727A52363042666C426E39326E4E7434526E786F66516661677743"
   const router=useRouter();
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPrompt, setShowPrompt] = useState(false)
   // Function to handle form submission
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Here you would handle the login logic
-    console.log('Form submitted');
-    router.push('/dashboard');
+    
+    let requestBody = JSON.stringify({
+      username: username,
+      password: password
+    })
+    await fetch("http://localhost:8080/hbs/auth/authenticate", {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: requestBody
+    })
+    .then(response => {
+      if(response.status == 403) {
+        setShowPrompt(true)
+      } else {
+        response.json().then(json => {
+          const jwt = json.token
+          const claims = decode(jwt)
+          const user = claims.sub
+          fetch(`http://localhost:8080/hbs/employee/type/${user}`)
+          .then(response => {
+            response.json().then(type => {
+              localStorage.setItem('jwt', jwt)
+              localStorage.setItem('type', type)
+            })
+          })
+        })
+        console.log("user authenticated");
+        router.push('/dashboard');
+      }
+    })
   };
 
   return (
@@ -25,14 +59,17 @@ const LoginPage = () => {
         <div className="input-group">
           <label htmlFor="username">Username</label>
           {/* <input type="text" id="username" name="username" required /> */}
-          <input type="text" id="username" name="username"/>
+          <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label>
           {/* <input type="password" id="password" name="password" required /> */}
-          <input type="password" id="password" name="password"/>
+          <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
         {/* <button type="submit" className='LoginBtn' onClick={() => router.push('/dashboard')}>Login</button> */}
+        {
+          showPrompt ? <label style={{color: 'red'}}>Incorrect username or password!</label> : null
+        }
         <button type="submit" className='LoginBtn'>Log in</button>
       </form>
     </div>
